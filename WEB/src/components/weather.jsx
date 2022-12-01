6e1009e6321683ec69b2ac3d98af2b1cc426f786
Shehzad <shehzaddiqbal@gmail.com>
2022-12-01 21:38:43 +0500

    update

--- WEB/src/components/weather.jsx
+++ WEB/src/components/weather.jsx
@@ -1,10 +1,52 @@
-import React from 'react'
+import React from "react";
 
 const Weather = () => {
   return (
-    
-    <div>Weather</div>
-  )
-}
+    <>
+      <nav class="nav-bar">
+        <h1>Weather App</h1>
+      </nav>
+      <div class="body_padding_div">
+        <form onsubmit="myFunction(); return false" class="formm">
+          <input type="text" id="city" placeholder="Enter your city name" />
+          {/* <!-- <button type="submit">Get Weather</button> --> */}
+        </form>
+        <div class="main_container">
+          <div>
+            <img src="" alt="" id="weather_icon" />
+          </div>
+          <div id="tempC"></div>
+        </div>
+        <div class="flex">
+          <div id="city_name"></div>
+          <div id="weather_condition"></div>
+        </div>
 
-export default Weather
+        <div class="details">
+          <div class="left">
+            <div id="tempF"></div>
+            <br />
+            <div id="feels_like"></div>
+            <br />
+            <div id="humidity"></div>
+            <br />
+          </div>
+          <div class="right">
+            <div id="cloud"></div>
+            <div id="temp_f"></div>
+            <div id="wind"></div>
+            <div id="visibility"></div>
+            <br />
+            <div id="wind_speed"></div>
+          </div>
+        </div>
+      </div>
+
+      <footer>
+        <p>Developed by Shehzad</p>
+      </footer>
+    </>
+  );
+};
+
+export default Weather;
